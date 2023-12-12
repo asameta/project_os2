51f7fd6452f5bb2acc8963d9f8fdcf557f1ee8c1
asameta <ayaktaki.yolcu@gmail.com>
2023-12-12 11:40:54 +0300

    os2 v07

--- boot_time.sh
+++ boot_time.sh
@@ -1,7 +1,7 @@
-cat current_time.txt >> /home/project_os2/logs/timelog.txt
+cat /home/porject_os2/logs/current_time.txt >> /home/project_os2/logs/timelog.txt
 
 while [ 0 ]
 do
-  date '+%Y-%m-%d %X' > /home/project_os2/logs/current_time.txt
+  date '+%Y-%m-%d %X' > /home/project_os2/logs/current_time.txt 2&1
   sleep 1
 done
--- html/dist/data/save.php
+++
@@ -1,6 +0,0 @@
-<?php
-$ini_data = file_get_contents('php://input');
-file_put_contents('setup.ini', $ini_data);
-echo 'Success';
-
-?>
