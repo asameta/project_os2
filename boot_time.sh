f9f6bd761817483d8b70711d32f87935cd78a490
asameta <ayaktaki.yolcu@gmail.com>
2023-12-17 15:36:20 +0300

    os2 v08  bugs

--- boot_time.sh
+++ boot_time.sh
@@ -2,6 +2,6 @@ cat /home/project_os2/logs/current_time.txt >> /home/project_os2/logs/timelog.tx
 
 while [ 0 ]
 do
-  date '+%Y-%m-%d %X' > /home/project_os2/logs/current_time.txt 2&1
+  date '+%Y-%m-%d %X' > /home/project_os2/logs/current_time.txt
   sleep 1
 done
