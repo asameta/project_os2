084697388e37f62f1d070433efc46c53d8588ecc
asameta <ayaktaki.yolcu@gmail.com>
2024-02-16 11:57:11 +0300

    os2 v09 - volume flow 3600x to m3/h fixing

--- boot_time.sh
+++ boot_time.sh
@@ -1,13 +1,2 @@
-if test -f /home/project_os2/logs; then
-  cd /home/project_os2/logs
-else
-  sudo mkdir /home/project_os2/logs
-fi
-
-cd /home/project_os2/logs/
-if test -f /home/project_os2/logs/current_time.txt; then
-  cat current_time.txt >> /home/project_os2/logs/timelog.txt
-else
-  sudo uptime -s '+%Y-%m-%d %X' > /home/project_os2/logs/current_time.txt
-fi
-date '+%Y-%m-%d %X' > /home/project_os2/logs/current_time.txt
+cat /home/project_os2/logs/current_time.log >> /home/project_os2/logs/timelog.log 2>&1
+date '+%Y-%m-%d %X' > /home/project_os2/logs/current_time.log
